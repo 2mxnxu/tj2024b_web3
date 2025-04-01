4095263664f10f951be490de9232cab49f396514
2ãmxnxu <leeeun099@naver.com>
2025-04-01 15:43:36 +0900

    엔터티, 리포지토리

---
+++ src/main/java/example/_과제/BookController.java
@@ -0,0 +1,30 @@
+package example._과제;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
+@RequiredArgsConstructor
+public class BookController {
+    private final BookService bookService;
+    @PostMapping
+    public boolean post(@RequestBody BookEntity bookentity) {
+        boolean result = bookService.post(bookentity);
+        return result;
+    }
+    @GetMapping
+    public List<BookEntity> get() {
+        return bookService.get();
+    }
+    @PutMapping boolean put(@RequestBody BookEntity bookentity) {
+        boolean result = bookService.put(bookentity);
+        return result;
+    }
+    @DeleteMapping boolean delete(@RequestParam int id) {
+        boolean result = bookService.delete(id);
+        return result;
+    }
+}
