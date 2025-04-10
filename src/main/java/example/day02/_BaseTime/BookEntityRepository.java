44928e49cac467696c86b135c7f9cdfbefaa291f
2ãmxnxu <leeeun099@naver.com>
2025-04-10 14:39:56 +0900

    JPA, JAVA 참조관계

--- src/main/java/example/day02/_BaseTime/BookEntityRepository.java
+++
@@ -1,8 +0,0 @@
-package example.day02._BaseTime;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookEntityRepository extends JpaRepository<BookEntity, Integer> {
-}
